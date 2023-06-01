442ade61341fdd879dd1076bf275187ce22ecc5a
Marilyn Haber <mha@logisoft.io>
2023-06-01 20:21:31 +0300

    Add SignUp Create Endpoint

--- JobReady/Controllers/SignUpController.cs
+++ JobReady/Controllers/SignUpController.cs
@@ -5,6 +5,11 @@ namespace JobReady.Controllers
 {
     public class SignUpController : Controller
     {
+        private readonly JobReadyContext context;
+        public SignUpController(JobReadyContext context)
+        {
+            this.context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -19,7 +24,47 @@ namespace JobReady.Controllers
                 return RedirectToAction("Index","Login");
             }
             return View(model);
-           
+
         }
+
+        [HttpPost]
+        public IActionResult Create(UserAccountDetails source)
+        {
+            source.Validate();
+            var existing = (from x in context.UserAccounts
+                            where
+                            x.Email == source.Email
+                            && x.IsEmailVerified
+                            || x.Username == source.Username
+                            select x);
+            if(existing.Any())
+            {
+                throw new Exception("This user already exists");
+            }
+            var target = new UserAccount()
+            {
+                About = source.About,
+                AccountType = source.AccountType,
+                Email = source.Email,
+                UserDate = source.UserDate,
+                FullName = source.FullName,
+                Username = source.Username,
+                Password = source.Password,
+                PhoneNumber = source.PhoneNumber,
+                Headline = source.Headline,
+                Gender = source.Gender,
+                IsEmailVerified = source.IsEmailVerified,
+                IsVerified = source.IsVerified,
+                CreatedOn = DateTime.Now,
+                ModifiedOn = DateTime.Now,
+                Location = source.Location,
+                IndustryId = source.IndustryId
+            };
+            context.Add(target);
+            context.SaveChangesAsync();
+            return RedirectToAction("Index");
+            
+        }
+
     }
 }
