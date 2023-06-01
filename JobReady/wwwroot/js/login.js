c035bbfc48f9aa456d1d1db8937d961080bb6d99
Hasanjawich <hasanjawich4444@gmail.com>
2023-06-01 21:46:04 +0300

    finalizing front-end login

---
+++ JobReady/wwwroot/js/login.js
@@ -0,0 +1,12 @@
+﻿const eye = document.querySelector(".eye");
+const inputPassword = document.querySelector("#password");
+
+eye.addEventListener("click", () => {
+    if (eye.src.includes("eye-slash")) {
+        eye.src = "/icons/eye.svg";
+        inputPassword.type = "password";
+    }else{
+        eye.src = "/icons/eye-slash.svg";
+        inputPassword.type = "text"
+    }
+})
