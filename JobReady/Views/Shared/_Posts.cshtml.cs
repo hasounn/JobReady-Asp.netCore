1dfeb45be0adb9036d286f0aabe14437c90489a9
EtienneGhanem <etienneghanem25@gmail.com>
2023-06-02 15:09:18 +0300

    Finalizing front-end Home in User Side

---
+++ JobReady/Views/Shared/_Posts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JobReady.Views.Shared
+{
+    public class _PostsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ JobReady/wwwroot/js/homepage.js
@@ -0,0 +1,38 @@
+const maindiv = document.querySelector(".height-fix"),
+    posts = document.querySelector(".posts"),
+    layoutMainDiv = document.querySelector(".navbar"),
+    filterNewsItem = document.querySelectorAll(".filter-news-item");
+
+window.addEventListener("load", () => {
+    if (posts.children.length > 1) {
+        maindiv.style.height = "fit-content";
+        maindiv.style.paddingBottom = "20px";
+    } else {
+        maindiv.style.height = "100vh";
+        maindiv.style.paddingBottom = "0px";
+    }
+
+    if ((window.screen.width < 1024 && window.screen.width > 1000) ||
+        (window.screen.width <= 1000 && window.screen.width >= 769)) {
+        maindiv.style.paddingTop = layoutMainDiv.offsetHeight + 5 + "px";
+    }
+    else if (window.screen.width <= 768) {
+        maindiv.style.paddingTop = 12 + "px";
+    }
+    else if (window.screen.width >= 1024) {
+        maindiv.style.paddingTop = 0;
+    }
+})
+
+filterNewsItem.forEach(item => {
+    item.addEventListener("click", () => {
+        removeActiveItems();
+        item.classList.add("active");
+    })
+})
+
+const removeActiveItems = () => {
+    filterNewsItem.forEach(item => {
+        item.classList.remove("active");
+    })
+}
