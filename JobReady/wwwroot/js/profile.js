fd6511644d1dcf7424f67bac2ba3955db5335bf4
EtienneGhanem <etienneghanem25@gmail.com>
2023-06-05 21:36:16 +0300

    Adding first elements of the Profile Page

---
+++ JobReady/wwwroot/js/profile.js
@@ -0,0 +1,10 @@
+﻿const maindiv = document.querySelector(".height-fix"),
+    divs = document.querySelector(".all-divs");
+
+window.addEventListener("load", () => {
+    if (divs.children.length > 0) {
+        maindiv.style.height = "fit-content";
+    } else {
+        maindiv.style.height = "100vh";
+    }
+})
