5d25213c5f071b86052bf1d1e8b27e1819504ec8
EtienneGhanem <etienneghanem25@gmail.com>
2023-06-06 02:04:33 +0300

    Finishing frontend Profile Page

--- JobReady/wwwroot/js/profile.js
+++ JobReady/wwwroot/js/profile.js
@@ -8,3 +8,43 @@ window.addEventListener("load", () => {
         maindiv.style.height = "100vh";
     }
 })
+
+var tagCloud = TagCloud('.skills', skills, {
+
+    // Sphere radius in px
+    radius: 150,
+
+    // animation speed
+    // slow, normal, fast
+    maxSpeed: 'normal',
+    initSpeed: 'normal',
+
+    // Rolling direction [0 (top) , 90 (left), 135 (right-bottom)] 
+    direction: 135,
+
+    // interaction with mouse or not [Default true (decelerate to rolling init speed, and keep rolling with mouse).]
+    keep: true
+
+});
+
+// Giving color to each text in sphere
+var color = '#77cfff ';
+document.querySelector('.skills').style.color = color;
+
+
+var carouselWidth = $('.carousel-inner').width();
+var cardWidth = $(".carousel-item").width();
+var scrollPosition = 0;
+$('.carousel-control-next').on("click", () => {
+    if (scrollPosition < (carouselWidth - (cardWidth * 0.25))) {
+        scrollPosition = scrollPosition + cardWidth;
+        $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 800)
+    }
+})
+
+$('.carousel-control-prev').on("click", () => {
+    if (scrollPosition > 0) {
+        scrollPosition = scrollPosition - cardWidth;
+        $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 800)
+    }
+})
