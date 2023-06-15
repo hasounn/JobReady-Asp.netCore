2ef29dbc9e8d07b059218511b3b0f5d4e394fddb
EtienneGhanem <etienneghanem25@gmail.com>
2023-06-15 15:42:43 +0300

    Finalizing Company Profile

---
+++ JobReady/wwwroot/js/profilecomp.js
@@ -0,0 +1,10 @@
+﻿const maindiv = document.querySelector(".height-fix"),
+    divs = document.querySelector(".all-divs");
+
+window.addEventListener("load", () => {
+    if (divs.children.length > 0) {
+        maindiv.style.height = document.body.scrollHeight + "px";
+    } else {
+        maindiv.style.height = "100vh";
+    }
+})
