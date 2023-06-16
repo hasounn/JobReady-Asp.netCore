717804510fa8b0dca3f04b9a79880783338f9a5a
Hasanjawich <hasanjawich4444@gmail.com>
2023-06-16 18:35:55 +0300

    addNotification

---
+++ JobReady/wwwroot/js/notifications.js
@@ -0,0 +1,17 @@
+﻿
+const maindiv = document.querySelector(".height-fix");
+
+window.addEventListener("load", () => {
+
+    maindiv.style.height = document.body.scrollHeight + "px";
+})
+
+$(document).ready(function () {
+
+    $('.category').click(function () {
+        var category = $(this).data('category');
+        $('.category-notification').hide();
+        $('.' + category + '-notification').show();
+    });
+});
+
