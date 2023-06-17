b43b85359a826824e2da40b9f78622e737198c8a
Hasanjawich <hasanjawich4444@gmail.com>
2023-06-18 00:31:20 +0300

    add Dtos

---
+++ JobReady/Data/DTO/FileLinkDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobReady.Data.DTO
+{
+    public class FileLinkDetails
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long ContentSize { get; set; }
+        public virtual System.Byte[] ContentHash { get; set; }
+        public ObjectType? ObjectType { get; set; }
+        public long? ObjectId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedById { get; set; }
+        public UserAccountDetails CreatedBy { get; set; }
+    }
+}
