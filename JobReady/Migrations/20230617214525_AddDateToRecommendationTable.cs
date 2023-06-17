3e9d60ceae15167833c4233fd99d725fdca2df7e
MarilynHb <65784907+MarilynHb@users.noreply.github.com>
2023-06-18 00:47:36 +0300

    Add Missing Dtos (#61)
    Co-authored-by: Hasanjawich <hasanjawich4444@gmail.com>

---
+++ JobReady/Migrations/20230617214525_AddDateToRecommendationTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobReady.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDateToRecommendationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RequestDate",
+                table: "Recommendation",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ResponseDate",
+                table: "Recommendation",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequestDate",
+                table: "Recommendation");
+
+            migrationBuilder.DropColumn(
+                name: "ResponseDate",
+                table: "Recommendation");
+        }
+    }
+}
