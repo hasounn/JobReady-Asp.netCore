127128de38309d0da754c4714a43d2573c67080e
EtienneGhanem <etienneghanem25@gmail.com>
2023-06-17 20:05:51 +0300

    ajax v2

--- JobReady/wwwroot/js/jobpost.js
+++ JobReady/wwwroot/js/jobpost.js
@@ -19,18 +19,19 @@ const addSkill = (e) => {
     let p = document.createElement("p");
     p.classList.add("skillset-item");
     p.textContent = skill;
-    if (skill !== "") {
-        skillSet.appendChild(p);
-        $.ajax({
-            type: "POST",
-            url: '@Url.Action("AddSkill", "JobPost")',
-            contentType: "application/json; charset=utf-8",
-            data: { skillId: skillInput.options[skillInput.selectedIndex].value },
-            dataType: "json",
-            success: function () { alert('Success'); },
-            error: function (e) { alert(e); }
-        });
-    }
+    $.ajax({
+        type: "POST",
+        url: '@Url.Action("AddSkill", "JobPost")',
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({ skillId: skillId }),
+        dataType: "json",
+        success: function () {
+            skillSet.appendChild(p);
+        },
+        error: function (e) {
+            alert("An error occurred: " + e.responseText);
+        }
+    });
     closeBtn.click();
     skillInput.value = "";
 }
