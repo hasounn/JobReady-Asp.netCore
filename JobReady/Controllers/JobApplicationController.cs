2b48215037c766cb749aba9a992e6f338d4025ed
Marilyn Haber <mha@logisoft.io>
2023-06-18 23:53:52 +0300

    add apply endpoint

--- JobReady/Controllers/JobApplicationController.cs
+++ JobReady/Controllers/JobApplicationController.cs
@@ -4,9 +4,30 @@ namespace JobReady.Controllers
 {
     public class JobApplicationController : Controller
     {
+        private readonly JobReadyContext context;
+        public JobApplicationController(JobReadyContext context)
+        {
+            this.context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Apply(JobApplicationDetails details)
+        {
+            if(ModelState.IsValid)
+            {
+                var application = new JobApplication()
+                {
+                    ApplicantId = this.User.Claims.First().Value,
+                    AppliedOn = DateTime.Now,
+                    JobPostId = details.JobPostId,
+                    LetterOfMotivation = details.LetterOfMotivation
+                };
+                context.JobApplication.Add(application);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
