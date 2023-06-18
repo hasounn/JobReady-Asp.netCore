ada915033d47e995755bd614943c3abbd9421adb
Hasanjawich <hasanjawich4444@gmail.com>
2023-06-18 19:19:23 +0300

    add JobPosts HomPage

--- JobReady/Controllers/HomeController.cs
+++ JobReady/Controllers/HomeController.cs
@@ -53,5 +53,30 @@ namespace JobReady.Controllers
                          }).AsEnumerable();
             return posts;
         }
+
+        [HttpGet]
+        public IEnumerable<JobPostDetails> GetJobPosts()
+        {
+            var posts = (from x in context.Post
+                         join y in context.FileLink on x.Id equals y.ObjectId into images
+                         from i in images.DefaultIfEmpty()
+                         where i == null || i.ObjectType == ObjectType.Post
+                         orderby x.CreatedOn descending
+                         select new PostDetails()
+                         {
+                             Id = x.Id,
+                             CreatedBy = new UserAccountDetails()
+                             {
+                                 Id = x.CreatedById,
+                                 Headline = x.CreatedBy.Headline,
+                                 Username = x.CreatedBy.UserName,
+                             },
+                             Content = x.Content,
+                             ImageId = i.Id,
+                             CreatedById = x.CreatedById,
+                             CreatedOn = x.CreatedOn,
+                         }).AsEnumerable();
+            return posts;
+        }
     }
 }
