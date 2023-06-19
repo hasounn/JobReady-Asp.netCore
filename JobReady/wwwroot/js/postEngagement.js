a40df7e3b221328551b0ca2cee168809ecae846a
EtienneGhanem <etienneghanem25@gmail.com>
2023-06-19 20:30:12 +0300

    ajax fix

--- JobReady/wwwroot/js/postEngagement.js
+++ JobReady/wwwroot/js/postEngagement.js
@@ -1,15 +1,16 @@
 ﻿const like = document.querySelector(".like"),
     share = document.querySelector(".share"),
     report = document.querySelector(".report"),
-    postId = document.getElementById("postId";
+    postId = document.getElementById("postId"),
+    countLikes = document.querySelector(".count-likes");
 
 
 like.addEventListener("click", () => {
     const path = like.querySelector(".like-button-path");
     const text = like.querySelector("p");
     if (path.getAttribute("fill") === "#FE1F1F") {
-        path.setAttribute("fill" ,"none");
-        path.setAttribute("stroke","#525657");
+        path.setAttribute("fill", "none");
+        path.setAttribute("stroke", "#525657");
         text.textContent = "Like";
         text.style.color = "#525657";
     } else {
@@ -17,20 +18,22 @@ like.addEventListener("click", () => {
             type: "POST",
             url: '/JobPost/LikePost',
             contentType: "application/json; charset=utf-8",
-            data: JSON.stringify({"postId":),
+            data: JSON.stringify(postId),
             dataType: "json",
             success: function (response) {
-                skillSet.appendChild(p);
+                if (response != false) {
+                    path.setAttribute("fill", "#FE1F1F");
+                    path.setAttribute("stroke", "#FE1F1F");
+                    text.textContent = "Liked";
+                    text.style.color = "#FE1F1F";
+                    countLikes.textContent = response;
+                }
             },
             error: function (xhr, status, error) {
                 // Handle error
                 alert("An error occurred: " + error);
             }
         })
-        path.setAttribute("fill" , "#FE1F1F");
-        path.setAttribute("stroke" , "#FE1F1F");
-        text.textContent = "Liked";
-        text.style.color = "#FE1F1F";
     }
 })
 
