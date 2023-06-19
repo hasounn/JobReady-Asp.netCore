975854a06e2394f59ccd449b42f0e52fa1226d1a
EtienneGhanem <etienneghanem25@gmail.com>
2023-06-19 20:30:12 +0300

    ajax fix

--- JobReady/wwwroot/js/postEngagement.js
+++ JobReady/wwwroot/js/postEngagement.js
@@ -1,7 +1,8 @@
 ﻿const like = document.querySelector(".like"),
     share = document.querySelector(".share"),
     report = document.querySelector(".report"),
-    postId = document.getElementById("postId";
+    postId = document.getElementById("postId"),
+    countLikes = document.querySelector(".count-likes");
 
 
 like.addEventListener("click", () => {
@@ -17,20 +18,22 @@ like.addEventListener("click", () => {
             type: "POST",
             url: '/JobPost/LikePost',
             contentType: "application/json; charset=utf-8",
-            data: JSON.stringify({"postId":),
+            data: JSON.stringify(postId),
             dataType: "json",
             success: function (response) {
-                skillSet.appendChild(p);
+                if (response != false) { 
+                path.setAttribute("fill", "#FE1F1F");
+                path.setAttribute("stroke", "#FE1F1F");
+                text.textContent = "Liked";
+                    text.style.color = "#FE1F1F";
+                    countLikes.textContent = response;
+                }
             },
             error: function (xhr, status, error) {
                 // Handle error
                 alert("An error occurred: " + error);
             }
         })
-        path.setAttribute("fill" , "#FE1F1F");
-        path.setAttribute("stroke" , "#FE1F1F");
-        text.textContent = "Liked";
-        text.style.color = "#FE1F1F";
     }
 })
 
