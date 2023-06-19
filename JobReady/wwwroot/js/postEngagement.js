9206dde914cf5e6a3ad2551086e2a18c0e754cfc
EtienneGhanem <etienneghanem25@gmail.com>
2023-06-19 21:15:30 +0300

    fix js

--- JobReady/wwwroot/js/postEngagement.js
+++ JobReady/wwwroot/js/postEngagement.js
@@ -1,53 +1,54 @@
-﻿const like = document.querySelector(".like"),
-    share = document.querySelector(".share"),
-    report = document.querySelector(".report"),
-    postId = document.getElementById("postId").value;
+﻿const likes = document.querySelectorAll(".like"),
+    shares = document.querySelectorAll(".share"),
+    reports = document.querySelectorAll (".report");
 
 
-like.addEventListener("click", () => {
-    const path = like.querySelector(".like-button-path");
-    const text = like.querySelector("p");
-    if (path.getAttribute("fill") === "#FE1F1F") {
-        $.ajax({
-            type: "POST",
-            url: '/Post/UnlikePost',
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(postId),
-            dataType: "json",
-            success: function (response) {
-                if (response != false) {
-                    path.setAttribute("fill", "none");
-                    path.setAttribute("stroke", "#525657");
-                    text.textContent = response;
-                    text.style.color = "#525657";
+likes.forEach(like => {like.addEventListener("click", () => {
+        const path = like.querySelector(".like-button-path");
+        const text = like.querySelector("p");
+    const postId = document.querySelector("#postId").value
+        if (path.getAttribute("fill") === "#FE1F1F") {
+            $.ajax({
+                type: "POST",
+                url: '/Post/UnlikePost',
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(postId),
+                dataType: "json",
+                success: function (response) {
+                    if (response != false) {
+                        path.setAttribute("fill", "none");
+                        path.setAttribute("stroke", "#525657");
+                        text.textContent = response;
+                        text.style.color = "#525657";
+                    }
+                },
+                error: function (xhr, status, error) {
+                    // Handle error
+                    alert("An error occurred: " + error);
                 }
-            },
-            error: function (xhr, status, error) {
-                // Handle error
-                alert("An error occurred: " + error);
-            }
-        })
-    } else {
-        $.ajax({
-            type: "POST",
-            url: '/Post/LikePost',
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(postId),
-            dataType: "json",
-            success: function (response) {
-                if (response != false) { 
-                path.setAttribute("fill", "#FE1F1F");
-                path.setAttribute("stroke", "#FE1F1F");
-                    text.textContent = response;
-                    text.style.color = "#FE1F1F";
+            })
+        } else {
+            $.ajax({
+                type: "POST",
+                url: '/Post/LikePost',
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(postId),
+                dataType: "json",
+                success: function (response) {
+                    if (response != false) { 
+                    path.setAttribute("fill", "#FE1F1F");
+                    path.setAttribute("stroke", "#FE1F1F");
+                        text.textContent = response;
+                        text.style.color = "#FE1F1F";
+                    }
+                },
+                error: function (xhr, status, error) {
+                    // Handle error
+                    alert("An error occurred: " + error);
                 }
-            },
-            error: function (xhr, status, error) {
-                // Handle error
-                alert("An error occurred: " + error);
-            }
-        })
-    }
+            })
+        }
+    })
 })
 
 
