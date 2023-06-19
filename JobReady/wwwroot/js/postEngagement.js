21816e3498a45a0255ee5dc3ebe5c8ee9f3d65c0
EtienneGhanem <etienneghanem25@gmail.com>
2023-06-19 20:16:32 +0300

    add ajax for LikePost

--- JobReady/wwwroot/js/postEngagement.js
+++ JobReady/wwwroot/js/postEngagement.js
@@ -1,24 +1,39 @@
 ﻿const like = document.querySelector(".like"),
     share = document.querySelector(".share"),
-    report = document.querySelector(".report");
+    report = document.querySelector(".report"),
+    postId = document.getElementById("postId";
 
 
 like.addEventListener("click", () => {
     const path = like.querySelector(".like-button-path");
     const text = like.querySelector("p");
-    console.log("Like");
     if (path.getAttribute("fill") === "#FE1F1F") {
         path.setAttribute("fill" ,"none");
         path.setAttribute("stroke","#525657");
         text.textContent = "Like";
         text.style.color = "#525657";
     } else {
+        $.ajax({
+            type: "POST",
+            url: '/JobPost/LikePost',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({"postId":),
+            dataType: "json",
+            success: function (response) {
+                skillSet.appendChild(p);
+            },
+            error: function (xhr, status, error) {
+                // Handle error
+                alert("An error occurred: " + error);
+            }
+        })
         path.setAttribute("fill" , "#FE1F1F");
         path.setAttribute("stroke" , "#FE1F1F");
         text.textContent = "Liked";
         text.style.color = "#FE1F1F";
     }
 })
+
 
 $(".reportText").each(function () {
     this.setAttribute("style", "height: 40px;overflow-y:hidden;");
