08312e86f26d2a164f92a6417e8349ae2750298d
Marilyn Haber <mha@logisoft.io>
2023-06-22 01:17:32 +0300

    add mailjet package

---
+++ JobReady/Provider/MessageConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace JobReady;
+
+public class MessageConfiguration
+{
+    public string PrivateApiKey { get; set; }
+    public string PublicApiKey { get; set; }       
+    public string FromAddress { get; set; }
+    public string FromDisplayName { get; set; }
+    public bool IsTestMode { get; set; }
+}
