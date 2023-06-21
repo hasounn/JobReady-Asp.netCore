1779b6cafdb51f226004bad102afc20d34189723
Marilyn Haber <mha@logisoft.io>
2023-06-22 00:01:10 +0300

    add notification endpoint

--- JobReady/Controllers/NotificationsController.cs
+++ JobReady/Controllers/NotificationsController.cs
@@ -4,74 +4,65 @@ namespace JobReady.Controllers
 {
     public class NotificationsController : Controller
     {
-        public IActionResult Index()
+        private readonly JobReadyContext context;
+        public NotificationsController(JobReadyContext context)
         {
-            //var notifications = GetNotifications();
-            //ViewData["Notifications"] = notifications;
-            return View();
+            this.context = context;
         }
 
-        //    private List<Notification> GetNotifications()
-        //    {
-        //        var postEngagements = GetPostEngagements();
-        //        var recommendations = GetRecommendations();
-        //        var followers = GetFollowers();
+        public IActionResult Index(string view = "_Engagement")
+        {
+            var notification = new NotificationDetails()
+            {
+                Engagements = GetAllEngagements(),
+                Partial = view,
+            };
+            return View(notification);
+        }
 
-        //        var notifications = new List<Notification>();
+        public IActionResult GetEngagements()
+        {
+            return Index("_Engagement");
+        }
 
-        //        foreach (var postEngagement in postEngagements)
-        //        {
-        //            notifications.Add(new Notification { Category = "all", Message = postEngagement.Message, Time = postEngagement.Time });
-        //        }
+        private IEnumerable<NotificationEngagementDetails> GetAllEngagements()
+        {
+            var engagements = (from x in context.PostEngagement
+                               where x.Post.CreatedById == this.User.Claims.First().Value
+                               orderby x.Id descending
+                               select new NotificationEngagementDetails()
+                               {
+                                   CreatedBy = new UserAccountDetails()
+                                   {
+                                       Id = x.CreatedById,
+                                       Username = x.CreatedBy.UserName,
+                                       Headline = x.CreatedBy.Headline
+                                   },
+                                   EngagementType = x.EngagementType,
+                                   CreatedOn = x.CreatedOn,
+                                   Content = x.EngagementType == EngagementType.Like ? $"{x.CreatedBy.UserName} liked your post" : $"{x.CreatedBy.UserName} commented on your post",
+                                   PostedOn = $"{x.CreatedOn.ToShortDateString()} - {x.CreatedOn.ToShortTimeString()}",
+                               }).ToList();
 
-        //        foreach (var recommendation in recommendations)
-        //        {
-        //            notifications.Add(new Notification { Category = "recommendations", Message = recommendation.Message, Time = recommendation.Time });
-        //        }
+            engagements.AddRange(from x in context.Follower
+                                 where x.FollowingId == this.User.Claims.First().Value
+                                 orderby x.Id descending
+                                 select new NotificationEngagementDetails()
+                                 {
+                                     CreatedBy = new UserAccountDetails()
+                                     {
+                                         Id = x.UserAccountId,
+                                         Username = x.UserAccount.UserName,
+                                         Headline = x.UserAccount.Headline
+                                     },
+                                     EngagementType = EngagementType.Follow,
+                                     CreatedOn = x.FollowedOn,
+                                     Content = $"{x.UserAccount.UserName} followed you!",
+                                     PostedOn = $"{x.FollowedOn.ToShortDateString()} - {x.FollowedOn.ToShortTimeString()}",
+                                 });
 
-        //        foreach (var follower in followers)
-        //        {
-        //            notifications.Add(new Notification { Category = "follow-requests", Message = follower.Message, Time = follower.Time });
-        //        }
-
-        //        return notifications;
-        //    }
-
-        //    private List<PostEngagement> GetPostEngagements()
-        //    {
-        //        // Replace this with your logic to retrieve post engagements from the database or any other data source
-        //        var postEngagements = new List<PostEngagement>
-        //        {
-        //            new PostEngagement { Message = "User 1 liked your post.", Time = "1 hour ago" },
-        //            new PostEngagement { Message = "User 2 commented on your post.", Time = "2 hours ago" }
-        //        };
-
-        //        return postEngagements;
-        //    }
-
-        //    private List<Recommendation> GetRecommendations()
-        //    {
-        //        // Replace this with your logic to retrieve recommendations from the database or any other data source
-        //        var recommendations = new List<Recommendation>
-        //        {
-        //            new Recommendation { Message = "User 3 accepted your recommendation request.", Time = "3 hours ago" },
-        //            new Recommendation { Message = "User 4 recommended you a new book.", Time = "4 hours ago" }
-        //        };
-
-        //        return recommendations;
-        //    }
-
-        //    private List<Follower> GetFollowers()
-        //    {
-        //        // Replace this with your logic to retrieve followers from the database or any other data source
-        //        var followers = new List<Follower>
-        //        {
-        //            new Follower { Message = "User 5 has requested to follow you!", Time = "5 hours ago" },
-        //            new Follower { Message = "User 6 started following you.", Time = "6 hours ago" }
-        //        };
-
-        //        return followers;
-        //    }
+            return engagements.OrderByDescending(x => x.CreatedOn).AsEnumerable();
+        }
     }
-   
+
 }
