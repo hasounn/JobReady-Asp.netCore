2612b4cd0505d6c961ba8452d66eadd41c729ee6
EtienneGhanem <etienneghanem25@gmail.com>
2023-06-23 00:05:02 +0300

    add educations modal

--- JobReady/Enums/Enums.cs
+++ JobReady/Enums/Enums.cs
@@ -67,27 +67,65 @@ public enum ObjectType
 
 public enum DegreeType
 {
+    [Display(Name = "Bachelor of Arts")]
     BachelorOfArts,
+
+    [Display(Name = "Bachelor of Science")]
     BachelorOfScience,
+
+    [Display(Name = "Bachelor of Business Administration")]
     BachelorOfBusinessAdministration,
+
+    [Display(Name = "Bachelor of Engineering")]
     BachelorOfEngineering,
+
+    [Display(Name = "Bachelor of Education")]
     BachelorOfEducation,
+
+    [Display(Name = "Bachelor of Fine Arts")]
     BachelorOfFineArts,
+
+    [Display(Name = "Master of Arts")]
     MasterOfArts,
+
+    [Display(Name = "Master of Science")]
     MasterOfScience,
+
+    [Display(Name = "Master of Business Administration")]
     MasterOfBusinessAdministration,
+
+    [Display(Name = "Master of Education")]
     MasterOfEducation,
+
+    [Display(Name = "Master of Engineering")]
     MasterOfEngineering,
+
+    [Display(Name = "Master of Fine Arts")]
     MasterOfFineArts,
+
+    [Display(Name = "Doctor of Philosophy")]
     DoctorOfPhilosophy,
+
+    [Display(Name = "Doctor of Medicine")]
     DoctorOfMedicine,
+
+    [Display(Name = "Doctor of Education")]
     DoctorOfEducation,
+
+    [Display(Name = "Juris Doctor")]
     JurisDoctor,
+
+    [Display(Name = "Doctor of Dental Surgery")]
     DoctorOfDentalSurgery,
+
+    [Display(Name = "Doctor of Dental Medicine")]
     DoctorOfDentalMedicine,
+
+    [Display(Name = "Associate of Arts")]
     AssociateOfArts,
-    AssociateOfScience,
-    Other
+
+    [Display(Name = "Associate of Science")]
+    AssociateOfScience
 }
 
 public enum SearchType
--- JobReady/wwwroot/js/editprofile.js
+++ JobReady/wwwroot/js/editprofile.js
@@ -22,3 +22,17 @@ $(".about-textarea").each(function () {
         }, 0);
     }
 });
+
+$('.textarea-description').each(function () {
+    this.setAttribute("style", "height: 40px; overflow-y:auto;");
+}).on("input", function () {
+    this.style.height = "auto";
+    this.style.height = (this.scrollHeight) + "px";
+}).on("keydown", function (e) {
+    if (e.keyCode === 8 || e.keyCode === 46) { // Check if backspace or delete key is pressed
+        setTimeout(() => {
+            this.style.height = "auto";
+            this.style.height = (this.scrollHeight) + "px";
+        }, 0);
+    }
+});
