a7925175c6c8bab48a60e67358e8754c3ed7d792
Hasanjawich <hasanjawich4444@gmail.com>
2023-06-23 23:06:28 +0300

    add change password

---
+++ JobReady/wwwroot/js/Settings.js
@@ -0,0 +1,8 @@
+﻿function toggleWindow() {
+    var window = document.getElementById("window");
+    if (window.style.display === "none") {
+        window.style.display = "block";
+    } else {
+        window.style.display = "none";
+    }
+}
