b1bf54488138a467d1412f367e0cb7fc8984ded1
EtienneGhanem <etienneghanem25@gmail.com>
2023-06-24 19:27:24 +0300

    add modal

--- JobReady/Controllers/RecommendationController.cs
+++ JobReady/Controllers/RecommendationController.cs
@@ -26,5 +26,26 @@ namespace JobReady.Controllers
             var image = "/icons/tick-white.svg";
             return Ok(new {image});
         }
+
+        [HttpGet]
+        public IActionResult RejectRecommendation(long id)
+        {
+            var instructorId = (from x in context.UserAccount
+                                where x.Id == this.User.Claims.First().Value
+                                && x.AccountType == UserAccountType.Instructor
+                                select x).FirstOrDefault();
+            if(instructorId == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var target = (from x in context.Recommendation
+                          where x.Id == id
+                          select x).FirstOrDefault();
+            target.Status = RecommendationStatus.Rejected;
+            target.ResponseDate = DateTime.Now;
+            context.SaveChanges();
+            return RedirectToAction("Index", "Notifications", new { view = "_RecommendationView" });
+        }
     }
 }
