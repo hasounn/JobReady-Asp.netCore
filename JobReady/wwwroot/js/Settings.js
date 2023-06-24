5caba78b0a91f9bef6d1fa80d6c0bceae4e5fd94
EtienneGhanem <etienneghanem25@gmail.com>
2023-06-24 14:47:38 +0300

    fix UI

--- JobReady/wwwroot/js/Settings.js
+++
@@ -1,8 +0,0 @@
-﻿function toggleWindow() {
-    var window = document.getElementById("window");
-    if (window.style.display === "none") {
-        window.style.display = "block";
-    } else {
-        window.style.display = "none";
-    }
-}
