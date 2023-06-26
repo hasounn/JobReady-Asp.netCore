59cd80d3667918c2bd139105925b161846def2e9
Marilyn Haber <mha@logisoft.io>
2023-06-27 00:32:05 +0300

    add integration

--- JobReady/Controllers/SignUpController.cs
+++ JobReady/Controllers/SignUpController.cs
@@ -62,6 +62,7 @@ namespace JobReady.Controllers
                
                 if (result.Succeeded)
                 {
+
                     // Optionally, you can sign in the user after registration
                     await signInManager.SignInAsync(newUser, isPersistent: false);
 
@@ -101,6 +102,58 @@ namespace JobReady.Controllers
             }
 
             return View(details);
+        }
+
+        private async void MailJetTest()
+        {
+            var config = (from x in context.Configuration
+                          where x.Id == 1
+                          select new MessageConfiguration()
+                          {
+                              PrivateApiKey = x.PrivateKey,
+                              PublicApiKey = x.PublicKey,
+                              FromAddress = "marilyn.haber@st.ul.edu.lb",
+                              FromDisplayName = "JobReady",
+                              IsTestMode = false
+                          }).First();
+
+            var userEmail = (from x in context.Users where x.UserName == this.User.Identity.Name select x.Email).First();
+            var sendProvider = new MailJetProvider(config.PublicApiKey, config.PrivateApiKey, config.FromAddress, config.FromDisplayName , config.IsTestMode);
+
+            var message = new ReportMessage()
+            {
+                Subject = "Welcome to JobReady - Your Path to Success Starts Here!",
+                Body = @"
+
+Hey there!
+
+Welcome to JobReady, the platform that connects talented individuals like you with amazing career opportunities. We are thrilled to have you join our community of motivated professionals and companies seeking top talent.
+
+At JobReady, we understand the importance of finding the right opportunities to shape your future. Our mission is to empower you in your job search, provide valuable resources, and streamline the hiring process, making it easier for you to land your dream job.
+
+As a new member of JobReady, you now have access to a wide range of features and tools designed to enhance your career journey. Here are some key highlights:
+
+1. Create an Impressive Profile: Showcase your skills, experience, and achievements to make a strong impression on recruiters and potential employers.
+
+2. Discover Exciting Job Opportunities: Explore our extensive job database tailored to your preferences and career aspirations. Find positions that match your skills and interests effortlessly.
+
+3. Connect with Industry Professionals: Expand your professional network by connecting with like-minded individuals, industry experts, and recruiters who can open doors to new opportunities.
+
+4. Personalized Recommendations: Receive tailored job recommendations based on your profile, skills, and preferences, ensuring you never miss out on relevant opportunities.
+
+5. Stay Updated with Notifications: Be the first to know about new job openings, interview requests, and networking events through our timely notifications.
+
+We are committed to your success and are continuously working to provide you with the best platform and resources to accelerate your career growth. If you have any questions, concerns, or feedback, our support team is here to assist you every step of the way.
+
+Once again, welcome to JobReady! We're excited to have you on board and look forward to witnessing your achievements as you embark on this new chapter of your professional journey.
+
+Best regards,
+
+The JobReady Team",
+
+            Recipient = userEmail,
+            };
+            await sendProvider.SendMessage(message, CancellationToken.None);
         }
     }
 }
---
+++ JobReady/Provider/SendProvider.cs
@@ -0,0 +1,32 @@
+﻿namespace JobReady
+{
+    public abstract class SendProvider
+    {
+        protected SendProvider(string fromAddress, string fromDisplayName)
+        {
+            if (fromAddress == null) throw new Exception("'From' Address is required");
+            this.FromAddress = fromAddress;
+            this.FromDisplayName = fromDisplayName;
+        }
+        public string FromAddress { get; }
+        public string FromDisplayName { get; }
+        public abstract Task SendMessage(ReportMessage source, CancellationToken cancel);
+    }
+
+    public interface IMessagingProviderFactory
+    {
+        SendProvider CreateEmailProvider(MessageConfiguration dbConfig, string fromAddress, string fromDisplayName, bool isTestMode);
+    }
+    public class MessagingProviderFactory : IMessagingProviderFactory
+    {
+        public SendProvider CreateEmailProvider(MessageConfiguration dbConfig, string fromAddress, string fromDisplayName, bool isTestMode)
+        {
+            return new MailJetProvider(
+                privateApiKey: dbConfig.PrivateApiKey,
+                publicApiKey: dbConfig.PublicApiKey,
+                fromAddress: fromAddress,
+                fromDisplayName: fromDisplayName,
+                isTestMode: isTestMode);
+        }
+    }
+}
