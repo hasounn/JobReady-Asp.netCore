4cd54bcd7a36fda0b15821c90c6fae97f517e18a
EtienneGhanem <etienneghanem25@gmail.com>
2023-07-14 21:37:42 +0300

    add js

--- JobReady/wwwroot/js/logout.js
+++ JobReady/wwwroot/js/logout.js
@@ -1,3 +1,5 @@
-﻿window.onload = ()=>{
+﻿window.onload = () => {
+    var backlen = history.length;
+    history.go(-backlen);
     window.location.href = window.location.href.split("Logout/")[0]+"Login/";
 }
