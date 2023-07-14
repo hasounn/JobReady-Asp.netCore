a6176b8db1ecd10b2fa6d20d7da07f95e3097451
EtienneGhanem <etienneghanem25@gmail.com>
2023-07-14 20:57:29 +0300

    add logout script for back button

--- JobReady/Controllers/SignUpController.cs
+++ JobReady/Controllers/SignUpController.cs
@@ -130,6 +130,31 @@ namespace JobReady.Controllers
 
                 Recipient = "mh.marilynhaber@gmail.com",
             };
+
+            //Hey there!
+
+            //Welcome to JobReady, the platform that connects talented individuals like you with amazing career opportunities. We are thrilled to have you join our community of motivated professionals and companies seeking top talent.
+
+            //At JobReady, we understand the importance of finding the right opportunities to shape your future. Our mission is to empower you in your job search, provide valuable resources, and streamline the hiring process, making it easier for you to land your dream job.
+
+            //As a new member of JobReady, you now have access to a wide range of features and tools designed to enhance your career journey. Here are some key highlights:
+
+            //1. Create an Impressive Profile: Showcase your skills, experience, and achievements to make a strong impression on recruiters and potential employers.
+
+            //2. Discover Exciting Job Opportunities: Explore our extensive job database tailored to your preferences and career aspirations. Find positions that match your skills and interests effortlessly.
+
+            //3. Connect with Industry Professionals: Expand your professional network by connecting with like-minded individuals, industry experts, and recruiters who can open doors to new opportunities.
+
+            //4. Personalized Recommendations: Receive tailored job recommendations based on your profile, skills, and preferences, ensuring you never miss out on relevant opportunities.
+
+            //5. Stay Updated with Notifications: Be the first to know about new job openings, interview requests, and networking events through our timely notifications.
+
+            //We are committed to your success and are continuously working to provide you with the best platform and resources to accelerate your career growth. If you have any questions, concerns, or feedback, our support team is here to assist you every step of the way.
+
+            //Once again, welcome to JobReady! We're excited to have you on board and look forward to witnessing your achievements as you embark on this new chapter of your professional journey.
+
+            //Best regards,
+
             await sendProvider.SendMessage(message, CancellationToken.None);
         }
     }
