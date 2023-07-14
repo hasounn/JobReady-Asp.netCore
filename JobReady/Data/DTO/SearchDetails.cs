20fdb2aeab1f484e697915b6649b910fb8c5c796
EtienneGhanem <etienneghanem25@gmail.com>
2023-07-14 22:36:08 +0300

    push branch

--- JobReady/Data/DTO/SearchDetails.cs
+++ JobReady/Data/DTO/SearchDetails.cs
@@ -4,6 +4,7 @@ public class SearchDetails
 {
     public string SearchText { get; set; }
     public SearchType ResponseType { get; set; }
+    public string Type { get; set; }
     public IEnumerable<UserAccountDetails> Users { get; set; }
     public IEnumerable<PostDetails> Posts { get; set; }
     public IEnumerable<JobPostDetails> JobPosts { get; set; }
--- JobReady/wwwroot/js/search.js
+++ JobReady/wwwroot/js/search.js
@@ -1,5 +1,8 @@
 const maindiv = document.querySelector(".height-fix"),
-searchDiv = document.querySelector(".searchDiv");
+    searchDiv = document.querySelector(".searchDiv"),
+    filterBtns = document.querySelectorAll(".filter-btn"),
+    ;
+
 
 window.addEventListener("load", () => {
     if (searchDiv.children.length > 0) {
@@ -8,3 +11,20 @@ window.addEventListener("load", () => {
         maindiv.style.height = "100vh";
     }
 });
+
+filterBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        $.ajax({
+            type: "POST",
+            url: '/Search/ApplyFilter',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({ request: { SearchText: $(".search-input").val(), Type:btn.textContent } }),
+            dataType: "json",
+            success: function (response) {
+            },
+            error: function (xhr, status, error) {
+                alert("An error occurred: " + error);
+            }
+        })
+    })
+})
